0c79154d00184ad363a007088250206d50df6169
Vini Black <viniibl4ck@gmail.com>
2024-01-12 17:12:33 -0300

    section 1 lesson 2

--- scripts/run.js
+++ scripts/run.js
@@ -1,27 +1,8 @@
 async function main() {
   const gameContractFactory = await hre.ethers.getContractFactory("MyEpicGame");
-  const gameContract = await gameContractFactory.deploy(
-    ["Anitta", "Ronaldinho Gaúcho", "Zeca Pagodinho"],
-    [
-      "https://i.imgur.com/gC5qXsl.png",
-      "https://i.imgur.com/0PvxtwP.png",
-      "https://i.imgur.com/Pj8lHpM.png",
-    ],
-    [100, 200, 300], // Pontos de vida
-    [100, 50, 25] // Dando de ataque
-  );
+  const gameContract = await gameContractFactory.deploy();
   console.log("Contrato implantado no endereço:", gameContract.target);
-
-  let txn;
-  // Só temos três personagens.
-  // Uma NFT com personagem no index 2 da nossa array.
-  txn = await gameContract.mintCharacterNFT(2);
-  await txn.wait();
-
-  // Pega o valor da URI da NFT
-  let returnedTokenUri = await gameContract.tokenURI(1);
-  console.log("Token URI:", returnedTokenUri);
-}
+};
 
 main().catch((error) => {
   console.error(error);
