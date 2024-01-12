3d77e1a6fa277675d82ee13b569471eed1655172
Vini Black <viniibl4ck@gmail.com>
2024-01-12 17:07:14 -0300

    section 1 lesson 4

--- contracts/MyEpicGame.sol
+++ contracts/MyEpicGame.sol
@@ -1,49 +1,151 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+// Contrato NFT para herdar.
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+// Funcoes de ajuda que o OpenZeppelin providencia.
+import {Counters} from "@openzeppelin/contracts/utils/Counters.sol";
+import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
+
+// Helper que escrevemos para codificar em Base64
+import "./libraries/Base64.sol";
+
 import "hardhat/console.sol";
 
-contract MyEpicGame {
-  // Nos vamos segurar os atributos dos nossos personagens em uma
-  //struct. Sinta-se livre para adicionar o que quiser como um
-  //atributo! (ex: defesa, chance de critico, etc).
-  struct CharacterAttributes {
-    uint characterIndex;
-    string name;
-    string imageURI;
-    uint hp;
-    uint maxHp;
-    uint attackDamage;
-  }
-  // Uma pequena array vai nos ajudar a segurar os dados padrao dos
-  // nossos personagens. Isso vai ajudar muito quando mintarmos nossos
-  // personagens novos e precisarmos saber o HP, dano de ataque e etc.
-  CharacterAttributes[] defaultCharacters;
-
-  // Dados passados no contrato quando ele for criado inicialmente,
-  // inicializando os personagens.
-  // Vamos passar esse valores do run.js
-  constructor(
-    string[] memory characterNames,
-    string[] memory characterImageURIs,
-    uint[] memory characterHp,
-    uint[] memory characterAttackDmg
-  )
-  {
-    // Faz um loop por todos os personagens e salva os valores deles no
-    // contrato para que possamos usa-los depois para mintar as NFTs
-    for(uint i = 0; i < characterNames.length; i += 1) {
-      defaultCharacters.push(CharacterAttributes({
-        characterIndex: i,
-        name: characterNames[i],
-        imageURI: characterImageURIs[i],
-        hp: characterHp[i],
-        maxHp: characterHp[i],
-        attackDamage: characterAttackDmg[i]
-      }));
-
-      CharacterAttributes memory c = defaultCharacters[i];
-      console.log("Personagem inicializado: %s com %s de HP, img %s", c.name, c.hp, c.imageURI);
+// Nosso contrato herda do ERC721, que eh o contrato padrao de NFT!
+contract MyEpicGame is ERC721 {
+    struct CharacterAttributes {
+        uint characterIndex;
+        string name;
+        string imageURI;
+        uint hp;
+        uint maxHp;
+        uint attackDamage;
     }
-  }
+
+    // O tokenId eh o identificador unico das NFTs, eh um numero
+    // que vai incrementando, Ex: 0, 1, 2, 3, etc...
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    CharacterAttributes[] defaultCharacters;
+
+    // Criamos um mapping do tokenId => atributos das NFTs.
+    mapping(uint256 => CharacterAttributes) public nftHolderAttributes;
+
+    // Um mapping de um endereco => tokenId das NFTs, nos da um
+    // jeito facil de armazenar o dono da NFT e referenciar ele depois.
+    mapping(address => uint256) public nftHolders;
+
+    constructor(
+        string[] memory characterNames,
+        string[] memory characterImageURIs,
+        uint[] memory characterHp,
+        uint[] memory characterAttackDmg
+    )
+        // Embaixo, voce tambem pode ver que adicionei um simbolo especial para identificar nossas NFTs
+        // Esse eh o nome e o simbolo do nosso token, ex Ethereum ETH.
+        // Eu chamei o meu de Heroes e HERO. Lembre-se, um NFT eh soh um token!
+        ERC721("Heroes", "HERO")
+    {
+        for (uint i = 0; i < characterNames.length; i += 1) {
+            defaultCharacters.push(
+                CharacterAttributes({
+                    characterIndex: i,
+                    name: characterNames[i],
+                    imageURI: characterImageURIs[i],
+                    hp: characterHp[i],
+                    maxHp: characterHp[i],
+                    attackDamage: characterAttackDmg[i]
+                })
+            );
+
+            CharacterAttributes memory c = defaultCharacters[i];
+
+            // O uso do console.log() do hardhat nos permite 4 parametros em qualquer order dos seguintes tipos: uint, string, bool, address
+
+            console.log(
+                "Personagem inicializado: %s com %s de HP, img %s",
+                c.name,
+                c.hp,
+                c.imageURI
+            );
+        }
+
+        // Eu incrementei tokenIds aqui para que minha primeira NFT tenha o ID 1.
+        // Mais nisso na aula!
+        _tokenIds.increment();
+    }
+
+    // Usuarios vao poder usar essa funcao e pegar a NFT baseado no personagem que mandarem!
+    function mintCharacterNFT(uint _characterIndex) external {
+        // Pega o tokenId atual (começa em 1 já que incrementamos no constructor).
+        uint256 newItemId = _tokenIds.current();
+
+        // A funcao magica! Atribui o tokenID para o endereço da carteira de quem chamou o contrato.
+
+        _safeMint(msg.sender, newItemId);
+
+        // Nos mapeamos o tokenId => os atributos dos personagens. Mais disso abaixo
+
+        nftHolderAttributes[newItemId] = CharacterAttributes({
+            characterIndex: _characterIndex,
+            name: defaultCharacters[_characterIndex].name,
+            imageURI: defaultCharacters[_characterIndex].imageURI,
+            hp: defaultCharacters[_characterIndex].hp,
+            maxHp: defaultCharacters[_characterIndex].maxHp,
+            attackDamage: defaultCharacters[_characterIndex].attackDamage
+        });
+
+        console.log(
+            "Mintou NFT c/ tokenId %s e characterIndex %s",
+            newItemId,
+            _characterIndex
+        );
+
+        // Mantem um jeito facil de ver quem possui a NFT
+        nftHolders[msg.sender] = newItemId;
+
+        // Incrementa o tokenId para a proxima pessoa que usar.
+        _tokenIds.increment();
+    }
+
+    function tokenURI(
+        uint256 _tokenId
+    ) public view override returns (string memory) {
+        CharacterAttributes memory charAttributes = nftHolderAttributes[
+            _tokenId
+        ];
+
+        string memory strHp = Strings.toString(charAttributes.hp);
+        string memory strMaxHp = Strings.toString(charAttributes.maxHp);
+        string memory strAttackDamage = Strings.toString(
+            charAttributes.attackDamage
+        );
+
+        string memory json = Base64.encode(
+            abi.encodePacked(
+                '{"name": "',
+                charAttributes.name,
+                " -- NFT #: ",
+                Strings.toString(_tokenId),
+                '", "description": "Esta NFT da acesso ao meu jogo NFT!", "image": "',
+                charAttributes.imageURI,
+                '", "attributes": [ { "trait_type": "Health Points", "value": ',
+                strHp,
+                ', "max_value":',
+                strMaxHp,
+                '}, { "trait_type": "Attack Damage", "value": ',
+                strAttackDamage,
+                "} ]}"
+            )
+        );
+
+        string memory output = string(
+            abi.encodePacked("data:application/json;base64,", json)
+        );
+
+        return output;
+    }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -11,6 +11,16 @@ async function main() {
     [100, 50, 25] // Dando de ataque
   );
   console.log("Contrato implantado no endereço:", gameContract.target);
+
+  let txn;
+  // Só temos três personagens.
+  // Uma NFT com personagem no index 2 da nossa array.
+  txn = await gameContract.mintCharacterNFT(2);
+  await txn.wait();
+
+  // Pega o valor da URI da NFT
+  let returnedTokenUri = await gameContract.tokenURI(1);
+  console.log("Token URI:", returnedTokenUri);
 }
 
 main().catch((error) => {
