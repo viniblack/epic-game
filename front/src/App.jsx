8e1168398295b4432c8c6d174c01aa445cce7d5c
Vini Black <viniibl4ck@gmail.com>
2024-01-17 11:44:50 -0300

    section 3 lesson 2

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,35 +1,121 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/*
+ * Nós vamos precisar usar estados agora! Não esqueça de importar useState
+ */
+import { useEffect, useState } from "react";
+import "./App.css";
+import twitterLogo from "./assets/twitter-logo.svg";
 
-function App() {
-  const [count, setCount] = useState(0)
+// Constantes
+const TWITTER_HANDLE = "Web3dev_";
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+
+const App = () => {
+  /*
+   * Só uma variável de estado que vamos usar para armazenar a carteira pública do usuário.
+   */
+  const [currentAccount, setCurrentAccount] = useState(null);
+
+  /*
+   * Já que esse método vai levar um tempo, lembre-se de declará-lo como async
+   */
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log("Eu acho que você não tem a metamask!");
+        return;
+      } else {
+        console.log("Nós temos o objeto ethereum", ethereum);
+
+        /*
+         * Checa se estamos autorizados a acessar a carteira do usuário.
+         */
+        const accounts = await ethereum.request({ method: "eth_accounts" });
+
+        /*
+         * Usuário pode ter múltiplas contas autorizadas, pegamos a primeira se estiver ali!
+         */
+        if (accounts.length !== 0) {
+          const account = accounts[0];
+          console.log("Carteira conectada::", account);
+          setCurrentAccount(account);
+        } else {
+          console.log("Não encontramos uma carteira conectada");
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  /*
+   * Implementa o seu método connectWallet aqui
+   */
+  const connectWalletAction = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Instale a MetaMask!");
+        return;
+      }
+
+      /*
+       * Método chique para pedir acesso para a conta.
+       */
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      /*
+       * Boom! Isso deve escrever o endereço público uma vez que autorizarmos Metamask.
+       */
+      console.log("Contectado", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App">
+      <div className="container">
+        <div className="header-container">
+          <p className="header gradient-text">⚔️ Batalhas no Metaverso ⚔️</p>
+          <p className="sub-text">Junte os amigos e proteja o Metaverso!!</p>
+          <div className="connect-wallet-container">
+            <img
+              src="https://i.imgur.com/NqlaaTJ.gif"
+              alt="Nascimento Gif"
+            />
+            {/*
+             * Botão que vamos usar para disparar a conexão da carteira. Não esqueça de adicionar o evento onClick para chamar seu método!
+             */}
+            <button
+              className="cta-button connect-wallet-button"
+              onClick={connectWalletAction}
+            >
+              Conecte sua carteira para começar
+            </button>
+          </div>
+        </div>
+        <div className="footer-container">
+          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+          <a
+            className="footer-text"
+            href={TWITTER_LINK}
+            target="_blank"
+            rel="noreferrer"
+          >{`built with @${TWITTER_HANDLE}`}</a>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
